12b8044911815eb316fcdcd853de427b85fc269a
Cam Herringshaw <camherringshaw@gmail.com>
2017-06-18 13:00:02 +0000

    Move db info into config file

--- database.py
+++ database.py
@@ -1,14 +1,14 @@
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
-from config import db_host, db_pass
+from config import db_host, db_pass, db_user, db_name
 
 Base = declarative_base()
 
-engine = create_engine("mysql+pymysql://cam:{}@{}/monica_py".format(db_pass, db_host),
+engine = create_engine("mysql+pymysql://{}:{}@{}/{}".format(db_user, db_pass, db_host, db_name),
                        connect_args= dict(host=db_host, port=3306), echo=True)
 
 class Contact(Base):
-    __tablename__ = 'contacts_1'
+    __tablename__ = 'contacts'
     id = Column(Integer, primary_key=True)
     name = Column(String(40))
 
