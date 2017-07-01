003f43a0d8aca4ec94345de563b03367279f933d
Cam <camherringshaw@gmail.com>
2017-07-01 09:53:33 -0400

    Trying to get circle ci to work with a database

---
+++ config.py
@@ -0,0 +1,14 @@
+import configparser
+
+config = configparser.ConfigParser()
+
+config.read('databases.toml')
+try:
+    default = config.get('default', {})
+except:
+    default = {}
+
+db_host = default.get('host', '127.0.0.1')
+db_pass = default.get('pass', '')
+db_user = default.get('user', 'root')
+db_name = default.get('name', 'circle-test')
